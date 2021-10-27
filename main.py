e5e1609132ade13e64e194466a8694d184730ddf
anton708 <59750039+anton708@users.noreply.github.com>
2021-10-27 15:59:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/<name>', methods=['GET', 'POST'])
 def aws_flask_app(name):
-    return "Hello, {}!, I'm the AWS_FLASK_APP!".format(name)
+    user_data = request.form["body"]
+    return "Hello, {}!, I'm the AWS_FLASK_APP!".format(name) + "The data that you entered is: {}\n".format(user_data)
 
 
 if __name__ == '__main__':
