0617edc797880bae5ee9d2305023c2aaf830c108
QuantOpsNB <48415417+QuantOps@users.noreply.github.com>
2021-10-26 21:18:10 -0400

    Modified route to app to be based on typed string

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def aws_flask_app():
-    return "Hello! I'm the AWS_FLASK_APP!"
+@app.route('/<name>', methods=['GET', 'POST'])
+def aws_flask_app(name):
+    return "Hello, {}!, I'm the AWS_FLASK_APP!".format(name)
 
 
 if __name__ == '__main__':
