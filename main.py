550c3a518f6c981260fffd4bd4e79d1565cec310
anton708 <59750039+anton708@users.noreply.github.com>
2021-10-27 16:36:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 
 @app.route('/<name>', methods=['GET', 'POST'])
 def aws_flask_app(name):
-    user_data = request.form["body"]
-    return "Hello, {}!, I'm the AWS_FLASK_APP!".format(name) + "The data that you entered is: {}\n".format(user_data)
+    if request.method=='GET':
+        return "Hello, {}! I'm the AWS_FLASK_APP! \n".format(name)
+    elif request.method=='POST':
+        user_data = request.form["body"]
+        res = {'status': '200'}
+        return "Hello, {}! I'm the AWS_FLASK_APP! ".format(name) + "The data that you entered is: {}\n".format(user_data)
 
 
 if __name__ == '__main__':
