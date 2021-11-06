f2bd2f0b1542eb65502f70a61e5f18f0f784afb2
QuantOpsNB <48415417+QuantOps@users.noreply.github.com>
2021-11-06 12:38:42 -0400

    Modified route to app to be based on typed string
    added entry point for ngrams and tokenize methods

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import nltk
 from nltk.util import ngrams
+
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('averaged_perceptron_tagger')
-
 
 app = Flask(__name__)
 
@@ -33,35 +33,28 @@ def aws_flask_app():
     def service6(user_sentence):
         return "You have requested service six.\n"
 
-
-
     # Make a nice output that tells the user how to submit a POST reqest
-    if request.method=='GET':
+    if request.method == 'GET':
         return "Hello! I'm the AWS_FLASK_APP! \n"
-    
+
     # user_sentence is the sentence the user inputted. user_service is a number or a list of comma 
     # separated numbers asking for a particular service
-    elif request.method=='POST':
+    elif request.method == 'POST':
         user_sentence = request.form["Sentence"]
         user_services = request.form["Service"]
-        
-        
-        # Split list into individual services
-        services = str.split(user_services,",")
 
-        
+        # Split list into individual services
+        services = str.split(user_services, ",")
+
         res = {'status': '200'}
-        
+
         # Calls correct service depending on user input
-        methods = {service1:"1", service2:"2", service3:"3", service4:"4", service5:"5", service6:"6"}
+        methods = {service1: "1", service2: "2", service3: "3", service4: "4", service5: "5", service6: "6"}
         string = "The sentence you have entered is: {}.\n".format(user_sentence)
         for service in services:
             position = list(methods.values()).index(service)
             string = string + (list(methods.keys())[position](user_sentence))
         return string
-          
-
-
 
 
 @app.route("/n_grams/<num>/<text>/")
@@ -73,18 +66,16 @@ def run_ngrams(text, num):
         return "Num must be a number"
     if num <= 0:
         return "Num must greater than 0"
-    n_grams = ngrams(text_list,num)
+    n_grams = ngrams(text_list, num)
     return ' '.join([','.join(grams) for grams in n_grams])
 
 
 @app.route("/tokenize/<text>/")
 def get_tokens_and_tags(text):
-   tokens = nltk.word_tokenize(text)
+    tokens = nltk.word_tokenize(text)
     tags = nltk.pos_tag(tokens)
     return ' '.join(['='.join(tag) for tag in tags])
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-
-
